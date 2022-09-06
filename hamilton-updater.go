be2a293a126e0fde568d206d52a495cc2ffad1fd
Ryan Punt <ryan@mirum.org>
2022-09-05 23:00:59 -0500

    working results parsing

--- hamilton-updater.go
+++ hamilton-updater.go
@@ -3,9 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"time"
 )
 
@@ -56,7 +57,17 @@ type race_status struct {
 		ReplayBaseUrl string
 		SeasonContextUIState int
 	}
-	RaceResults []struct{
+	RaceResults []struct {
+		DriverTLA string
+		DriverFirstName string
+		DriverLastName string
+		TeamName string
+		PositionNumber string
+		RaceTime string
+		TeamColourCode string
+		GapToLeader string
+		DriverImage string
+		DriverNameFormat string
 	}
 	Race struct {
 		MeetingCountryName string
@@ -65,6 +76,16 @@ type race_status struct {
 		MeetingEndDate time.Time
 	}
 	SeasonYearImage string
+	SessionLinkSets struct {
+    ReplayLinks []struct {
+			Session string
+			Text string
+			Url string
+			LinkType string
+		}
+	}
+	CurrentRaceStatus string
+	Winner string
 }
 
 func getResults(URL string) race_status{
@@ -84,7 +105,10 @@ func getResults(URL string) race_status{
 	}
 	defer response.Body.Close()
 
-	body, _ := ioutil.ReadAll(response.Body)
+	// body, _ := ioutil.ReadAll(response.Body)
+
+	body, err := os.ReadFile("../didhamiltonwin/api_results/20220904-132607.json")
+
 	res := race_status{}
 	jsonErr := json.Unmarshal(body, &res)
 	if jsonErr != nil {
@@ -100,31 +124,46 @@ func main() {
 	// g.checkout(branch)
 	// # g.pull
 
-	race := getResults("https://api.formula1.com/v1/event-tracker")
-	fmt.Println(race.Race.MeetingCountryName, "will begin on", race.Race.MeetingStartDate)
-
-	// resBytes := []byte(res)
-	// var results map[string]interface{}
-	// _ = json.Unmarshal(resBytes, &results)
-	// fmt.Println(results["seasonContext"].(string))
-
 	// race_status = JSON.parse(response.body)
 	// raceinfo = race_status['seasonContext']['timetables'].select { |hash| hash['description'] == 'Race' }.first
 	// raceStart = Time.parse("#{raceinfo['startTime']} #{raceinfo['gmtOffset']}").utc
+
+	race := getResults("https://api.formula1.com/v1/event-tracker")
 
 	// config = YAML.load_file("#{__dir__}/_config.yml")
 	// config['description'] = "#{race_status['race']['meetingCountryName']}: #{raceStart.strftime('%d %B %Y')}"
 	// File.write("#{__dir__}/_config.yml", config.to_yaml)
 
-	// case raceinfo['state']
-	// when 'upcoming'
-	// 	answer = "The race hasn't started yet"
-	// when 'started'
-	// 	answer = "They're racing now"
-	// when 'completed'
-	// 	winner = race_status['raceResults'].select { |hash| hash['positionNumber'] == '1' }.first
-	// 	answer = winner['driverLastName'].downcase == 'hamilton' ? 'YES' : 'NO'
-	// end
+	for _, event := range race.SeasonContext.Timetables {
+		if event.Description == "Race" {
+			switch event.State {
+			case "upcoming":
+				race.CurrentRaceStatus = "The race hasn't started yet"
+			case "started":
+				race.CurrentRaceStatus = "They're racing now"
+			case "completed":
+				race.CurrentRaceStatus = "They're done racing"
+				for _, result := range race.RaceResults {
+					position, err := strconv.Atoi(result.PositionNumber)
+					if err != nil {
+						fmt.Println("err:", err)
+					}
+					if position == 1 {
+						race.Winner = result.DriverTLA
+					}
+				}
+			}
+		}
+	}
+
+	fmt.Println("state:", race.CurrentRaceStatus)
+	fmt.Println("winner:", race.Winner)
+	if race.Winner == "HAM" {
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
+
 	// File.write("#{__dir__}/index.md", "# #{answer}")
 	// File.write("#{__dir__}/api_results/#{Time.now.strftime('%Y%m%d-%H%M%S')}.json", JSON.pretty_generate(race_status))
 
